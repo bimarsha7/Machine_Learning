5003de01db4daa5dde6d0d9e968d5339e7431f4e
bimarsha7 <bmrsbhandari@gmail.com>
2020-12-14 23:07:12 +0545

    file deleted

--- .logs/global_log.py
+++
@@ -1,8 +0,0 @@
-# Fri, 20 Mar 2020 11:46:44
-%logstop
-%logstart -ortq ~/.logs/nlp.py append
-%matplotlib inline
-import matplotlib
-import seaborn as sns
-sns.set()
-matplotlib.rcParams['figure.dpi'] = 144
